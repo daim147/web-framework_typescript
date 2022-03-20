0de2be3da4b1494690231d20689ee6b2dd897c2c
Husnain Syed <daimshah784@gmail.com>
2022-03-20 22:54:11 +0500

    some changes

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,2 @@
 import { User } from './models/User';
 const user = new User({ name: 'Husnain', age: 23 });
-
-// console.log(user.get('name'));
-
-user.on('Click', () => console.log('TRIGGERS'));
-user.on('Click', () => console.log('TRIGGERS'));
-user.on('Click', () => console.log(user.events));
-console.log(user);
-user.trigger('Click');
